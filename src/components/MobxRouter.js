9b303e4a211a3399ca0fae4c673e1237041003b4
Nick Ribal <elektronik2k5@gmail.com>
2017-11-08 12:22:03 +0700

    Remove unnecessary import

--- src/components/MobxRouter.js
+++ src/components/MobxRouter.js
@@ -1,5 +1,7 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {observer, inject} from 'mobx-react';
 
-const MobxRouter = ({store:{router}}) => <div>{router.currentView && router.currentView.component}</div>;
+const MobxRouter = ({ store: { router, }, }) => (
+  <div>{router.currentView && router.currentView.component}</div>
+);
 export default inject('store')(observer(MobxRouter));
